532b08a19b16f8e1827204cdb34e225d4c04cc19
Hangyu <supremeethan@brandeis.edu>
2019-03-17 23:29:48 -0400

    Test Theme

---
+++ 4cw/front-page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      left side
+    </div>
+    <div class="col">
+      right side
+    </div>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ 4cw/functions.php
@@ -0,0 +1,22 @@
+<?php
+//not working
+function load_stylesheet()
+{
+
+  wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.min.css', array(), false, 'all');
+  wp_enqueue_style('bootstrap');
+
+  wp_register_style('style', get_template_directory_uri().'/style.css', array(), false, 'all');
+  wp_enqueue_style('style');
+}
+
+add_action('wp_enqueue_scripts', 'load_stylesheet');
+
+function load_js()
+{
+  wp_register_script('customjs', get_template_directory_uri() . "/js/scirpts.js", '', 1, true);
+  wp_enqueue_script('customjs');
+
+}
+
+add_action('wp_enqueue_scripts', 'load_js');
